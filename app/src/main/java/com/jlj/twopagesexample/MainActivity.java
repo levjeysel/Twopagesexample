9891e6d3c24091f793eb93122fadfe9fd2eee35c
Levjeysel <levjeysel.j@gmail.com>
2023-07-14 10:28:15 +0530

    Initial Commit

---
+++ app/src/main/java/com/jlj/twopagesexample/MainActivity.java
@@ -0,0 +1,31 @@
+package com.jlj.twopagesexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Activity2.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
